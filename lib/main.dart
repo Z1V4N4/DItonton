7557d6385127901c6846bc7cfe3a9dedbacd5594
MuhammadHafid195150400111001 <zisnura20@student.ub.ac.id>
2022-07-17 09:34:14 +0700

    googlecrashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -26,12 +26,15 @@ import 'package:ditonton/presentation/provider/tvseries_now_airing_notifier.dart
 import 'package:ditonton/presentation/provider/tvseries_search_notifier.dart';
 import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:ditonton/presentation/provider/watchlist_tvseries_notifier.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ditonton/injection.dart' as di;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   di.init();
   runApp(MyApp());
 }
